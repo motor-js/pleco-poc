1fefdcd5efb0e84684d92e23c154f82bde4cb18f
lureilly1 <lureilly1@gmail.com>
2020-08-26 10:46:33 +0200

    layout

--- src/sections/MainContent.jsx
+++ src/sections/MainContent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from 'styled-components'
 import {
   // useScreenSize,
   CurrentSelections,
@@ -9,18 +10,23 @@ import {
   useScreenSize,
 } from "@motor-js/core";
 
+const StyledSelections = styled(CurrentSelections)`
+  width: 100%;
+  margin: 100px;
+`;
+
 const MainContent = () => {
 
   const { screen } = useScreenSize()
 
   //grid area rows
-  const rows =  ["60px","auto"]
+  const rows =  ["50px","30%", "30%"]
 
   //grid area cols
   const cols = {
     mobile: ["auto"],
     tablet: ["auto"],
-    desktop: ["0.2fr", "1fr"],
+    desktop: ["auto","auto","auto","auto","auto"],
     largeDesktop: ["0.2fr", "1fr"]
   }
 
@@ -29,8 +35,11 @@ const MainContent = () => {
     mobile: [["hd"],["mn"]],
     tablet: [["hd"],["mn"]],
     desktop:
-      [["sd","hd"],
-      ["sd", "mn"]],
+      [
+        ["sel","sel","sel","sel","sel"],
+        ["kpi1","kpi2","bar1","bar1","bar1"],
+        ["kpi3","kpi4","bar2","bar2","bar2"]
+      ],
     largeDesktop:
     [["sd","hd"],
     ["sd", "mn"]],
@@ -39,8 +48,23 @@ const MainContent = () => {
 
   return (
     <Box gridArea='main' width='100%'>
-     <Grid >
-
+      <Grid       
+        rows={rows}
+        columns= {cols.desktop}
+        areas={areas.desktop}
+        fill
+        border
+        gap='10px'
+      >
+        <Box gridArea='sel'>
+          <StyledSelections size='small' width='100%' maxHeight='40px' minHeight='40px'/>
+        </Box>
+        <Box gridArea='kpi1' backgroundColor='red'/>
+        <Box gridArea='kpi2' backgroundColor='yellow'/>
+        <Box gridArea='kpi3' backgroundColor='blue'/>
+        <Box gridArea='kpi4' backgroundColor='green'/>
+        <Box gridArea='bar1' backgroundColor='violet'/>
+        <Box gridArea='bar2' backgroundColor='orange'/>
      </Grid>
     </Box>
   );
