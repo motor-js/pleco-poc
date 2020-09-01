4c33bc280660d127a9c8fe84f23fa04e6de9e4eb
Alan Green <Alan_Green@Alans-MacBook-Pro.local>
2020-09-01 10:16:37 +0100

    KPIs and filters added

--- src/sections/FilterContent.jsx
+++ src/sections/FilterContent.jsx
@@ -1,13 +1,20 @@
-
 import React from "react";
 import { Filter, Button } from "@motor-js/core";
 
 const FilterContent = (props) => {
   return (
     <div>
-      <Filter width="100%" label="Claim Status" dimension={["Claim Status"]} />
-      <Filter width="100%" label="Claim Type" dimension={["Claim Type"]} />
-      <Filter width="100%" label="Broker" dimension={["BrokerName"]} />
+      <Filter
+        width="100%"
+        label="Employee Name"
+        dimension={["Employee Name"]}
+      />
+      <Filter width="100%" label="Roles" dimension={["Roles"]} />
+      <Filter width="100%" label="Employee" dimension={["Employee"]} />
+      <Filter width="100%" label="Task" dimension={["Task"]} />
+      <Filter width="100%" label="Sprint" dimension={["Sprint"]} />
+      <Filter width="100%" label="Year" dimension={["Year"]} />
+      <Filter width="100%" label="MonthYear" dimension={["MonthYear"]} />
       <Button type="clearSelections">Clear Selections</Button>
     </div>
   );
--- src/sections/MainContent.jsx
+++ src/sections/MainContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from 'styled-components'
+import styled from "styled-components";
 import {
   // useScreenSize,
   CurrentSelections,
@@ -16,16 +16,15 @@ const StyledSelections = styled(CurrentSelections)`
 `;
 
 const MainContent = () => {
-
-  const { screen } = useScreenSize()
+  const { screen } = useScreenSize();
 
   //grid area rows
-  const rows =  {
-    mobile: ["50px","repeat(4,1fr)"],
-    tablet: ["50px","repeat(4,1fr)"],
-    desktop: ["50px","30%", "30%"],
-    largeDesktop: ["50px","30%", "30%"]
-  }
+  const rows = {
+    mobile: ["50px", "repeat(4,1fr)"],
+    tablet: ["50px", "repeat(4,1fr)"],
+    desktop: ["50px", "30%", "30%"],
+    largeDesktop: ["50px", "30%", "30%"],
+  };
 
   //grid area cols
   const cols = {
@@ -33,58 +32,105 @@ const MainContent = () => {
     tablet: ["repeat(2,1fr)"],
     desktop: ["repeat(5,1fr)"],
     largeDesktop: ["repeat(5,1fr)"],
-  }
+  };
 
   //grid area
   const areas = {
     mobile: [
-      ["sel","sel"],
-      ["kpi1","kpi2"],
-      ["kpi3","kpi4"],
-      ["bar1","bar1"],
-      ["bar2","bar2",]
+      ["sel", "sel"],
+      ["kpi1", "kpi2"],
+      ["kpi3", "kpi4"],
+      ["bar1", "bar1"],
+      ["bar2", "bar2"],
     ],
-    tablet: 
-      [
-        ["sel","sel"],
-        ["kpi1","kpi2"],
-        ["kpi3","kpi4"],
-        ["bar1","bar1"],
-        ["bar2","bar2",]
-      ],
-    desktop:
-      [
-        ["sel","sel","sel","sel","sel"],
-        ["kpi1","kpi2","bar1","bar1","bar1"],
-        ["kpi3","kpi4","bar2","bar2","bar2"]
-      ],
-    largeDesktop:
-    [
-      ["sel","sel","sel","sel","sel"],
-      ["kpi1","kpi2","bar1","bar1","bar1"],
-      ["kpi3","kpi4","bar2","bar2","bar2"]
+    tablet: [
+      ["sel", "sel"],
+      ["kpi1", "kpi2"],
+      ["kpi3", "kpi4"],
+      ["bar1", "bar1"],
+      ["bar2", "bar2"],
     ],
-  }
-
+    desktop: [
+      ["sel", "sel", "sel", "sel", "sel"],
+      ["kpi1", "kpi2", "bar1", "bar1", "bar1"],
+      ["kpi3", "kpi4", "bar2", "bar2", "bar2"],
+    ],
+    largeDesktop: [
+      ["sel", "sel", "sel", "sel", "sel"],
+      ["kpi1", "kpi2", "bar1", "bar1", "bar1"],
+      ["kpi3", "kpi4", "bar2", "bar2", "bar2"],
+    ],
+  };
 
   return (
-    <Box gridArea='main' width='100%'>
-      <Grid       
+    <Box gridArea="main" width="100%">
+      <Grid
         rows={rows[screen]}
-        columns= {cols[screen]}
+        columns={cols[screen]}
         areas={areas[screen]}
         fill
       >
-        <Box gridArea='sel'>
-          <StyledSelections size='small' width='100%' maxHeight='40px' minHeight='40px'/>
+        <Box gridArea="sel">
+          <StyledSelections
+            size="small"
+            width="100%"
+            maxHeight="40px"
+            minHeight="40px"
+          />
         </Box>
-        <Box gridArea='kpi1' backgroundColor='red'/>
-        <Box gridArea='kpi2' backgroundColor='yellow'/>
-        <Box gridArea='kpi3' backgroundColor='blue'/>
-        <Box gridArea='kpi4' backgroundColor='green'/>
-        <Box gridArea='bar1' backgroundColor='violet'/>
-        <Box gridArea='bar2' backgroundColor='orange'/>
-     </Grid>
+        {/* <Box gridArea='kpi1' backgroundColor='red'/> */}
+        <KPI
+          margin="10px"
+          cols={["=SUM([Forcast Day])"]}
+          label="Forecast"
+          size="small"
+          // border={false}
+          backgroundColor="white"
+          gridArea="kpi1"
+          // qNumType="M"
+          // qNumFmt="#,##0"
+          // roundNum={true}
+          // precision={false}
+          // border={false}
+        />
+
+        <KPI
+          margin="10px"
+          cols={["=SUM([Hours])"]}
+          label="Hours"
+          size="small"
+          // border={false}
+          backgroundColor="white"
+          gridArea="kpi2"
+          // border={false}
+        />
+        <KPI
+          margin="10px"
+          cols={[
+            "=Num(COUNT({<[Issue Type]={'Sub-task'}>}[Issue Type]),'#,##0')",
+          ]}
+          label="Total Subtasks"
+          size="small"
+          // border={false}
+          backgroundColor="white"
+          gridArea="kpi3"
+          // border={false}
+        />
+        <KPI
+          margin="10px"
+          cols={[
+            "=COUNT({<[Issue Type]={'Sub-task'},[Status]={'Done'}>}[Issue Type])",
+          ]}
+          label="Completed Subtasks"
+          size="small"
+          // border={false}
+          backgroundColor="white"
+          gridArea="kpi4"
+          // border={false}
+        />
+        <Box gridArea="bar1" backgroundColor="violet" />
+        <Box gridArea="bar2" backgroundColor="orange" />
+      </Grid>
     </Box>
   );
 };
